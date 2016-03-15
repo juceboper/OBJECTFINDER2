da544e73eff953313a6209cf05fc4d000744a53d
juceboper <juceboper@hotmail.com>
2016-03-15 16:12:44 -0400

    Ajuste Principal 2

---
+++ AppObjectFinder/AppObjectFinder/Models/PrincipalModels.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppObjectFinder.Models
+{
+    public class PrincipalModels
+    {
+        public int P_nroObjetos { get; set; }       
+    }
+}
